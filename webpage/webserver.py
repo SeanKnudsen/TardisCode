49ee708b703a5f36d1c9a0631c58f85e7f7b8b72
Thom Gerdes <tgerdes@gmail.com>
2016-04-30 10:27:08 -0700

    Use links and redirects instead of javascript

--- webpage/webserver.py
+++ webpage/webserver.py
@@ -5,16 +5,20 @@ import serial
 from flask import Flask
 app = Flask(__name__)
 
-s = serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1)
 @app.route("/open/<door>")
 def hello(door):
     string = b'1 {}\r'.format(door)
-    s.write(string)
-    return ""
+    try:
+        with serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1) as ser:
+            ser.write(string)
+    except serial.serialutil.SerialException:
+        pass
+    return flask.redirect('/close_door.html')
 
 @app.route('/<filename>')
 def serve(filename):
     return flask.send_from_directory('static', filename)
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0")
+    print "hello world"
+    app.run(debug=True)
